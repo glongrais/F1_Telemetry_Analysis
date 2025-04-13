f541be5db7db59ee893e55f2b3a68b7cc0c7b9ca
Guillaume Longrais <guillaume.longrais@orange.fr>
2025-04-13 23:31:57 +0200

    Add core sessions context model and schema for F1 telemetry analysis

---
+++ dbt/f1_analytics/models/core/core__sessions_context.sql
@@ -0,0 +1,21 @@
+-- f1_analytics/models/core/core__sessions_context.sql
+WITH
+sessions AS (SELECT * FROM {{ ref('stg__sessions') }}),
+drivers AS (SELECT * FROM {{ ref('stg__drivers') }}),
+
+drivers_per_session AS (
+    SELECT
+        d.session_key,
+        GROUP_CONCAT(d.driver_number, ', ') AS driver_numbers
+    FROM drivers d
+    GROUP BY d.session_key
+)
+
+SELECT
+    s.session_key,
+    s.date_start,
+    s.date_end,
+    dps.driver_numbers
+FROM sessions s
+LEFT JOIN drivers_per_session dps
+    ON s.session_key = dps.session_key
