d3b00ba983930ed06caae06e99d4a8d368ed1059
Guillaume Longrais <guillaume.longrais@orange.fr>
2025-04-15 17:07:57 +0200

    Refactor watermarks SQL to include meeting_key in combined_keys and streamline the selection process

--- dbt/f1_analytics/models/watermarks.sql
+++ dbt/f1_analytics/models/watermarks.sql
@@ -12,22 +12,30 @@ last_pits AS (SELECT 'pits' AS table_name, MAX(meeting_key) AS meeting_key, MAX(
 last_race_control AS (SELECT 'race_control' AS table_name, MAX(meeting_key) AS meeting_key, MAX(session_key) AS session_key FROM {{ ref('stg__race_control') }}),
 last_stints AS (SELECT 'stints' AS table_name, MAX(meeting_key) AS meeting_key, MAX(session_key) AS session_key FROM {{ ref('stg__stints') }}),
 last_team_radio AS (SELECT 'team_radio' AS table_name, MAX(meeting_key) AS meeting_key, MAX(session_key) AS session_key FROM {{ ref('stg__team_radio') }}),
-last_weather AS (SELECT 'weather' AS table_name, MAX(meeting_key) AS meeting_key, MAX(session_key) AS session_key FROM {{ ref('stg__weather') }})
+last_weather AS (SELECT 'weather' AS table_name, MAX(meeting_key) AS meeting_key, MAX(session_key) AS session_key FROM {{ ref('stg__weather') }}),
+
+combined_keys AS (
+  SELECT
+    table_name,
+    meeting_key,
+    session_key
+  FROM last_meetings
+  UNION ALL SELECT * FROM last_sessions
+  UNION ALL SELECT * FROM last_laps
+  UNION ALL SELECT * FROM last_positions
+  UNION ALL SELECT * FROM last_car_data
+  UNION ALL SELECT * FROM last_drivers
+  UNION ALL SELECT * FROM last_intervals
+  UNION ALL SELECT * FROM last_locations
+  UNION ALL SELECT * FROM last_pits
+  UNION ALL SELECT * FROM last_race_control
+  UNION ALL SELECT * FROM last_stints
+  UNION ALL SELECT * FROM last_team_radio
+  UNION ALL SELECT * FROM last_weather
+)
 
 SELECT
   table_name,
-  meeting_key,
-  session_key
-FROM last_meetings
-UNION ALL SELECT * FROM last_sessions
-UNION ALL SELECT * FROM last_laps
-UNION ALL SELECT * FROM last_positions
-UNION ALL SELECT * FROM last_car_data
-UNION ALL SELECT * FROM last_drivers
-UNION ALL SELECT * FROM last_intervals
-UNION ALL SELECT * FROM last_locations
-UNION ALL SELECT * FROM last_pits
-UNION ALL SELECT * FROM last_race_control
-UNION ALL SELECT * FROM last_stints
-UNION ALL SELECT * FROM last_team_radio
-UNION ALL SELECT * FROM last_weather
+  COALESCE(meeting_key, 0) AS meeting_key,
+  COALESCE(session_key, 0) AS session_key
+FROM combined_keys
